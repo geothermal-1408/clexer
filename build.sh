2f92bfa7b6d7f30cc4284d6fb051457cec16a708
geothermal-1408 <savvy3526@gmail.com>
2026-03-16 08:20:08 +0530

    added wildcard support and parser build

--- build.sh
+++ build.sh
@@ -6,28 +6,40 @@ set -xe
 CC="clang"
 CFLAGS="-Wall -Wextra -std=c11 -pedantic -O2"
 
-SRC="lexer.c"
-OBJ="lexer.o"
-LIB="liblexer.a"
+SRC="*.c"
+# OBJ="lexer.o"
+# LIB="liblexer.a"
 
 build() {
     # compile source to object file
-    $CC $CFLAGS -c $SRC -o $OBJ
-
-    # create static library
-    ar rcs $LIB $OBJ
+    for file in $SRC; do
+	base_name="${file%.c}"
+	obj_file="${base_name}.o"
+	lib_file="lib${base_name}.a"
+	
+	$CC $CFLAGS -c "$file" -o "$obj_file"
+	ar rcs "$lib_file" "$obj_file"
+    done
 }
 
 test() {
-    $CC $CFLAGS -o bin/usage usage.c -L. -llexer
+    mkdir -p bin
+    $CC $CFLAGS -o bin/usage usage.c -L. -llexer 
 }
 
+parser() {
+    mkdir -p bin
+    $CC $CFLAGS $SRC test/parser_test.c -I. -o bin/parser_test -L. -llexer -lparser
+}
 case "$1" in
     build)
         build
         ;;
     test)
         test
+        ;;
+    parser)
+        parser
         ;;
     *)
         echo "Usage: $0 {build|test}"
