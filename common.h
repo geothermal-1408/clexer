8ec07fd92d9581f2a7423abdc9ef653c525e2875
geothermal-1408 <savvy3526@gmail.com>
2026-03-18 22:30:43 +0530

    removed column no

--- common.h
+++ common.h
@@ -1,13 +1,9 @@
+#include <stdio.h>
+
 #ifndef COMMON_H
 #define COMMON_H
 
-#ifdef __clang__
-  #define _COL_ __builtin_COLUMN()
-#else
-  #define _COL_ 0
-#endif
-
-#define TODO(fn) fprintf(stderr, "%s:%d:%d: warning: NOT IMPLEMENTED: %s\n", \
-    __FILE__, __LINE__, _COL_, #fn)
+#define TODO(fn) fprintf(stderr, "%s:%d: Warning: NOT IMPLEMENTED: %s\n", \
+    __FILE__, __LINE__, #fn)
 
 #endif
